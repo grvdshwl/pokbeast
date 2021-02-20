aaaa086e7cd49c08c2d184f6f9265bc286284ce9
grvdshwl <79152383+grvdshwl@users.noreply.github.com>
2021-02-21 04:59:55 +0530

    Updates

---
+++ precache-manifest.206d4860d744d96ccb9e86d0ca223018.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebf08b9aa53a907b9594",
+    "url": "/pokebeast/static/css/main.75453f5a.chunk.css"
+  },
+  {
+    "revision": "ebf08b9aa53a907b9594",
+    "url": "/pokebeast/static/js/main.ebf08b9a.chunk.js"
+  },
+  {
+    "revision": "93a500d176d0ee07322b",
+    "url": "/pokebeast/static/css/1.5aba4d23.chunk.css"
+  },
+  {
+    "revision": "93a500d176d0ee07322b",
+    "url": "/pokebeast/static/js/1.93a500d1.chunk.js"
+  },
+  {
+    "revision": "35052b56b4f21777f6dd",
+    "url": "/pokebeast/static/js/runtime~main.35052b56.js"
+  },
+  {
+    "revision": "53e5ddb4cd1242a273e11607d531e647",
+    "url": "/pokebeast/index.html"
+  }
+];
